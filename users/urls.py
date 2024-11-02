91822b5dd11fb40b7471571a335448fdc567dc90
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-11-02 20:40:52 +0300

    task 4 is done

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,15 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentListAPIView
 
 app_name = UsersConfig.name
 
 router = DefaultRouter()
 router.register(r"user_profile", UserViewSet, basename='user_profile')
 
-urlpatterns = []
+urlpatterns = [
+    path("payments/", PaymentListAPIView.as_view(), name="payments-list"),
+]
 urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method',)
