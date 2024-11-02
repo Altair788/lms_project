58242985262468ff180650a11a3d17572fb5f091
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-11-02 20:55:02 +0300

    add task is done

--- users/serializers.py
+++ users/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from users.models import User, Payment
 
 
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = "__all__"
-
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -16,3 +10,11 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, source='payment_set')
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
