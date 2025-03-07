bd70a5ac0f3891c9a74e74d0b8d6e31e52a4935f
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-03-07 20:56:06 +0300

    fixes: static

--- config/settings.py
+++ config/settings.py
@@ -108,11 +108,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 STATIC_URL = "static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] \
-#     if (os.path.exists(os.path.join(BASE_DIR, 'static'))
-#         and os.listdir(os.path.join(BASE_DIR, 'static'))) else []
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] \
+    if (os.path.exists(os.path.join(BASE_DIR, 'static'))
+        and os.listdir(os.path.join(BASE_DIR, 'static'))) else []
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
