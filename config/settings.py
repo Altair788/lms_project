f3d397d425a2d0f7568c49924e343bbd97c5299d
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-03-07 18:28:51 +0300

    fixes: первый коммит с настроенным CI/CD

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))  # Преобразуем в int, так как это число
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"  # Преобразование из строки в bool
