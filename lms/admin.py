2bfdbe29d09fc5e48056943cfeeb9da5d918eef1
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-03-07 22:10:54 +0300

    fixes: flake8: F401

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Course, Lesson, Subscription
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'last_updated']
+    search_fields = ['title', 'description']
+    list_filter = ['owner', 'last_updated']
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course', 'owner']
+    search_fields = ['title', 'description', 'course__title']
+    list_filter = ['course', 'owner']
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course']
+    search_fields = ['user__email', 'course__title']
+    list_filter = ['course']
